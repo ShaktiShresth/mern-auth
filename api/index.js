52a48b1a48ac102e7f44fa142aab9403723769c6
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-01-21 19:38:46 +0545

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
@@ -20,3 +22,4 @@ app.listen(3001, () => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
