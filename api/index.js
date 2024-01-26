cb64138b9eeef2535647c9f4196428b545edfd4b
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-01-26 16:38:06 +0545

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,12 +4,8 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
-
-const app = express();
-app.use(express.json());
-
-app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
@@ -19,6 +15,20 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
